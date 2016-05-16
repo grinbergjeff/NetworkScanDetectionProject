b29acdb0355487975f579804445e73915b889a9b
grinbergjeff <grinbergjeff@gmail.com>
2016-05-16 16:46:23 -0400

    Implementing Key Checker

---
+++ scanproject.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# Project 3 : Network Scan Detection
+# EECE 480F
+# By: Jeffrey Grinberg
+# Due May 22, 2016
+
+import re
+from optparse import OptionParser
+
+
+
+# Function Declarations will appear below:
+
+# Checking to see if the analytics are to be done real-time or with log files:
+parser = OptionParser()
+parser.add_option("--online", action="store_true", dest="realTime", default=False)
+
+
+
+
+
+
+
+
+
+# Main Code Execution Below:
